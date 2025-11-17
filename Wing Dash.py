cd36b44c8da2d328e883ff75722365d4cad81462
santosb2 <santosb2@berea.edu>
2025-11-17 08:38:26 -0500

    Started working on the whole layout on the game and Im missing one function. Just need to think about and besides that we're good.

--- Wing Dash.py
+++ Wing Dash.py
@@ -1,11 +1,39 @@
 from tkinter import *
-
+import random
+import time
 window = Tk()
 Game_window = Frame(window,width=400,height=600)
 
 
-def game():
+class bird:
+    def __init__(self,canvas,width,height):
+        self.canvas = canvas
+        self.width = width
+    def jump(self):
 
-def jump():
+    def draw(self,canvas):
+
+    def
+
+class pipe:
+    def __init__(self,canvas,width,height):
+        self.width = width
+        self.height = height
+    def draw(self,canvas):
+
+class game:
+    def __init__(self,canvas,width,height):
+
+    def spawn_pipe(self,pipe,width,height):
+
+    def check_collision(self,pipe):
+
+    def draw (self,canvas):
+
+    def restart(self):
+
+
+
+
 
 window.after("<space>",jump)
